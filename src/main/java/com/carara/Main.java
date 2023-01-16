23a91b37b37a45fafa5868c157063109d17065c6
Pedro Carara <cararax@gmail.com>
2023-01-16 13:19:41 -0300

    refactor: move file reading methods to a dedicated reading class

--- src/main/java/com/carara/Main.java
+++ src/main/java/com/carara/Main.java
@@ -3,85 +3,28 @@ package com.carara;
 import com.carara.model.Faturamento;
 import com.carara.model.Nota;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static com.carara.reader.FileReader.faturamentoReader;
+import static com.carara.reader.FileReader.notaReader;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        final String basePath = "src/main/resources/";
 
 
 //        ArrayList<Faturamento> faturamentoList = new ArrayList<>();
-        ArrayList<Nota> notaList = new ArrayList<>();
+        List<Nota> notaList = new ArrayList<>();
         List<Faturamento> faturamentoList = new ArrayList<>();
 
-        //metodo de leitura de faturamento, adicionar filtro por ano
-        try (BufferedReader reader = new BufferedReader(new FileReader(basePath + "faturamento.txt"))) {
+        faturamentoList = faturamentoReader(faturamentoList);
+        notaList = notaReader(notaList);
 
-            //remove linhas em branco
-            var linha = reader.lines().filter(l -> !l.isEmpty()).toList();
-            //pula primeira linha (cabeçalho)
-            linha = linha.subList(1, linha.size());
-            String[] dados /*= new String[9]*/;
-
-//            String linha;
-            for (String line : linha) {
-                /*String[]*/
-                dados = line.split(";");
-                String company = dados[0];
-                String mes = dados[1];
-                String ano = dados[2];
-                String dataParcela1 = dados[3];
-                String parcela1 = /*Double.parseDouble(*/dados[4];
-                String dataParcela2 = dados[5];
-                String parcela2 = /*Double.parseDouble(*/dados[6];
-                String dataParcela3 = dados[7];
-                String parcela3 = /*Double.parseDouble(*/dados[8];
-
-                Faturamento faturamento = new Faturamento(company, mes, ano, dataParcela1, parcela1, dataParcela2, parcela2, dataParcela3, parcela3);
-                faturamentoList.add(faturamento);
-                //return faturamentos.stream().collect(Collectors.groupingBy(Faturamento::getCompany));
-            }
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
-
-        //metodo de leitura de faturamento, adicionar filtro por ano
-        try (BufferedReader reader = new BufferedReader(new FileReader(basePath + "nota.txt"))) {
-
-            //remove linhas em branco
-            var linha = reader.lines().filter(l -> !l.isEmpty()).toList();
-            //pula primeira linha (cabeçalho)
-            linha = linha.subList(1, linha.size());
-            String[] dados /*= new String[9]*/;
-
-//            String linha;
-            for (String line : linha) {
-                /*String[]*/
-                dados = line.split(";");
-                String company = dados[0];
-                String mes = dados[1];
-                String ano = dados[2];
-                String valor = dados[3];
-                String dataEmissao = /*Double.parseDouble(*/dados[4];
-                String numeroNota = dados[5];
-
-
-                Nota nota = new Nota(company, mes, ano, valor, dataEmissao, numeroNota);
-                notaList.add(nota);
-//                return notas.stream().collect(Collectors.groupingBy(Notas::getCompany));
-            }
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
         faturamentoList.forEach(System.out::println);
         notaList.forEach(System.out::println);
+
 
     }
 }
---
+++ src/main/java/com/carara/reader/FileReader.java
@@ -0,0 +1,86 @@
+package com.carara.reader;
+
+import com.carara.model.Faturamento;
+import com.carara.model.Nota;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileReader {
+    public static final String basePath = "src/main/resources/";
+
+    public static List<Faturamento> faturamentoReader(List<Faturamento> faturamentoList) throws IOException {
+        //metodo de leitura de faturamento, adicionar filtro por ano
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(basePath + "faturamento.txt"))) {
+
+            //remove linhas em branco
+            var linha = reader.lines().filter(l -> !l.isEmpty()).toList();
+            //pula primeira linha (cabeçalho)
+            linha = linha.subList(1, linha.size());
+            String[] dados;
+
+//            String linha;
+            for (String line : linha) {
+                dados = line.split(";");
+                String company = dados[0];
+                String mes = dados[1];
+                String ano = dados[2];
+                String dataParcela1 = dados[3];
+                String parcela1 = dados[4];
+                String dataParcela2 = dados[5];
+                String parcela2 = dados[6];
+                String dataParcela3 = dados[7];
+                String parcela3 = dados[8];
+
+                Faturamento faturamento = new Faturamento(company, mes, ano, dataParcela1, parcela1, dataParcela2,
+                        parcela2, dataParcela3, parcela3);
+                faturamentoList.add(faturamento);
+
+//                return faturamentoList.stream().collect(Collectors.groupingBy(Faturamento::getCompany)).values().stream().flatMap(List::stream).collect(Collectors.toList());
+            }
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return faturamentoList.stream().collect(Collectors.groupingBy(Faturamento::getCompany)).values().stream().flatMap(List::stream).collect(Collectors.toList());
+    }
+
+    public static List<Nota> notaReader(List<Nota> notaList) throws IOException {
+        //metodo de leitura de faturamento, adicionar filtro por ano
+        //metodo de leitura de faturamento, adicionar filtro por ano
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(basePath + "nota.txt"))) {
+
+            //remove linhas em branco
+            var linha = reader.lines().filter(l -> !l.isEmpty()).toList();
+            //pula primeira linha (cabeçalho)
+            linha = linha.subList(1, linha.size());
+            String[] dados /*= new String[9]*/;
+
+//            String linha;
+            for (String line : linha) {
+                /*String[]*/
+                dados = line.split(";");
+                String company = dados[0];
+                String mes = dados[1];
+                String ano = dados[2];
+                String valor = dados[3];
+                String dataEmissao = /*Double.parseDouble(*/dados[4];
+                String numeroNota = dados[5];
+
+
+                Nota nota = new Nota(company, mes, ano, valor, dataEmissao, numeroNota);
+                notaList.add(nota);
+//                return notaList.stream().collect(Collectors.groupingBy(Nota::getCompany));
+//                return notaList.stream().collect(Collectors.groupingBy(Nota::getCompany)).values().stream().flatMap(List::stream).collect(Collectors.toList());
+
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return notaList.stream().collect(Collectors.groupingBy(Nota::getCompany)).values().stream().flatMap(List::stream).collect(Collectors.toList());
+
+    }
+}
