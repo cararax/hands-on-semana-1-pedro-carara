30cf0bfbd1577b23a77ae9c000196ec391581f3f
Pedro Carara <cararax@gmail.com>
2023-01-16 13:02:28 -0300

    wip: read resource files and instantiate an list of models

--- src/main/java/com/carara/Main.java
+++ src/main/java/com/carara/Main.java
@@ -1,49 +1,111 @@
 package com.carara;
 
-import java.io.IOException;
+import com.carara.model.Faturamento;
+import com.carara.model.Nota;
 
-import static com.carara.GeraRelatorio.teste;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        teste();
-    }
-//    public class GeraRelatorio {
-//        public static void main(String[] args) throws IOException {
-//            // Leitura dos arquivos CSV
-//            BufferedReader faturamento = new BufferedReader(new FileReader("src/main/resources/faturamento.txt"));
-//            BufferedReader notas = new BufferedReader(new FileReader("src/main/resources/notas.txt"));
-//
-//            // Mapa para armazenar os valores faturados por empresa
-//            Map<String, Double> valoresFaturados = new HashMap<>();
-//
-//            // Leitura dos dados dos arquivos de faturamento
+        final String basePath = "src/main/resources/";
+
+
+//        ArrayList<Faturamento> faturamentoList = new ArrayList<>();
+        ArrayList<Nota> notaList = new ArrayList<>();
+        List<Faturamento> faturamentoList = new ArrayList<>();
+
+        //metodo de leitura de faturamento, adicionar filtro por ano
+        try (BufferedReader reader = new BufferedReader(new FileReader(basePath + "faturamento.txt"))) {
+
+            //remove linhas em branco
+            var linha = reader.lines().filter(l -> !l.isEmpty()).toList();
+            //pula primeira linha (cabeçalho)
+            linha = linha.subList(1, linha.size());
+            String[] dados /*= new String[9]*/;
+
 //            String linha;
-//            while ((linha = faturamento.readLine()) != null) {
-//                String[] dados = linha.split(";");
-//                String empresa = dados[0];
-//                double valor = Double.parseDouble(dados[3]) + Double.parseDouble(dados[5]) + Double.parseDouble(dados[7]);
-//                valoresFaturados.put(empresa, valor);
-//            }
-//
-//            // Criação dos arquivos de saída
-//            FileWriter emConformidade = new FileWriter("emConformidade.csv");
-//            FileWriter emNaoConformidade = new FileWriter("emNaoConformidade.csv");
-//
-//            // Verificação de conformidade e escrita dos dados nos arquivos de saída
-//            while ((linha = notas.readLine()) != null) {
-//                String[] dados = linha.split(";");
-//                String empresa = dados[0];
-//                double valorNotas = Double.parseDouble(dados[3]);
-//                if (valoresFaturados.containsKey(empresa) && valoresFaturados.get(empresa) == valorNotas) {
-//                    emConformidade.write(empresa + ";" + valorNotas + "\n");
-//                } else {
-//                    emNaoConformidade.write(empresa + ";" + valorNotas + "\n");
-//                }
-//            }
-//            emConformidade.close();
-//            emNaoConformidade.close();
-//        }
-//    }
+            for (String line : linha) {
+                /*String[]*/
+                dados = line.split(";");
+                String company = dados[0];
+                String mes = dados[1];
+                String ano = dados[2];
+                String dataParcela1 = dados[3];
+                String parcela1 = /*Double.parseDouble(*/dados[4];
+                String dataParcela2 = dados[5];
+                String parcela2 = /*Double.parseDouble(*/dados[6];
+                String dataParcela3 = dados[7];
+                String parcela3 = /*Double.parseDouble(*/dados[8];
+
+                Faturamento faturamento = new Faturamento(company, mes, ano, dataParcela1, parcela1, dataParcela2, parcela2, dataParcela3, parcela3);
+                faturamentoList.add(faturamento);
+                //return faturamentos.stream().collect(Collectors.groupingBy(Faturamento::getCompany));
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        //metodo de leitura de faturamento, adicionar filtro por ano
+        try (BufferedReader reader = new BufferedReader(new FileReader(basePath + "nota.txt"))) {
+
+            //remove linhas em branco
+            var linha = reader.lines().filter(l -> !l.isEmpty()).toList();
+            //pula primeira linha (cabeçalho)
+            linha = linha.subList(1, linha.size());
+            String[] dados /*= new String[9]*/;
+
+//            String linha;
+            for (String line : linha) {
+                /*String[]*/
+                dados = line.split(";");
+                String company = dados[0];
+                String mes = dados[1];
+                String ano = dados[2];
+                String valor = dados[3];
+                String dataEmissao = /*Double.parseDouble(*/dados[4];
+                String numeroNota = dados[5];
+
+
+                Nota nota = new Nota(company, mes, ano, valor, dataEmissao, numeroNota);
+                notaList.add(nota);
+//                return notas.stream().collect(Collectors.groupingBy(Notas::getCompany));
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        faturamentoList.forEach(System.out::println);
+        notaList.forEach(System.out::println);
+
+    }
 }
+
+//        try(BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/faturamento.txt"))) {
+//
+//            var lines = reader.lines().filter(l -> !l.isEmpty()).toList();
+//            lines = lines.subList(1, lines.size());
+//            var values = new String[9];
+//
+//            for(String line : lines) {
+//                values = line.split(";");
+//
+////                if(Integer.parseInt(values[2]) == ano){
+//                    var faturamento = new Faturamento(values[0],
+//                            Integer.parseInt(values[1]), Integer.parseInt(values[2]));
+//                    faturamento.popular(values);
+//                    faturamentoList.add(faturamento);
+////                }
+//            }
+//
+//        } catch (Exception e) {
+//            LOGGER.info(e.getMessage());
+//        }
+//
+//        return faturamentoList.stream().collect(Collectors.groupingBy(Faturamento::getCompany));
+
